22635d8e19520aed819147eb6664a2f5bc46cf62
Данил Шабуков <danilshabukov8@gmail.com>
2023-11-01 05:49:13 +0400

    Add project files.

---
+++ Tyuiu.ShabukovDV.Sprint4.Task0.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabukovDV.Sprint4.Task0.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabukovDV.Sprint4.Task0.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf70a9ce-ab98-4025-8e4c-118cbc885341")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
